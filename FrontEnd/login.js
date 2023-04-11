67e58204cdd110dd984dcd9fe063836df7bde729
Benjamin Simon <benjamin.simon31000@hotmail.com>
2023-04-11 16:31:46 +0200

    final

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,61 +1,37 @@
-function userConexion() {
-    //On cible le bouton conexion
-    let myForm = document.getElementById("myForm");
-    //On met un evenement au click ou sur la touche "entrée"
-    myForm.addEventListener("submit", function (event) {
+// Récupération des informations d'authentification à partir du FORM de LOGIN.HTML
+const formInfos = document.querySelector("form");
 
-        let inputs = document.getElementsByTagName("input");
-        let myError;
-        //on vérifie que les champs sont remplies
-        for (let input of inputs) {
-            if (!input.value) {
-                myError = "Veuillez renseigner tous les champs";
-            }
-        }
-        //on retourne une erreur si les champs sont
-        if (myError) {
-            event.preventDefault()
-            document.getElementById("error").innerHTML = myError;
-            document.getElementById("error").style.color = "red";
-            return false;
-        }
-        else {
-            event.preventDefault()
-            //on récupere les infos dans le but de les envoyer
-            let user = {
-                email: event.target.querySelector("[name=email]").value,
-                password: event.target.querySelector("[name=password]").value
-            };
-            // Création de la charge utile au format JSON
-            let chargeUtile = JSON.stringify(user);
-            // Appel de la fonction fetch avec toutes les informations nécessaires
-            fetch("http://localhost:5678/api/users/login", {
-                method: "post",
-                headers: { "Content-Type": "application/json", "accept": "application/json" },
-                body: chargeUtile
-            })
-                //verifier qu'il a une réponse et la renvoie ne json
-                .then(function (response) {
-                    if (response.ok) {
-                        return response.json();
-                    }
-                })
-                //traitement de la réponse
-                .then(function (contenu) {
-                    //on nettoie le local storage
-                    localStorage.clear();
-                    // on met dans le local storage le Token et l'Id du user
-                    localStorage.setItem("responseToken", contenu.token);
-                    localStorage.setItem("responseId", contenu.userId);
-                    document.location.href = "./index.html";
-                })
-                // si l'authentification n'a pas fonctionné
-                .catch(function (error) {
-                    console.log(error);
-                    window.alert("Veuillez entrez des id corrects")
-                });
-        }
-    });
-}
+// Fonction d'authentification à partir des informations de l'API
 
-userConexion();
+formInfos.addEventListener("submit", async function(event) {
+    event.preventDefault();
+    // Création de l'objet "userInfos"
+	const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+	const userInfos = {email,password};
+
+	// Envoi de ces informations sur l'API "LOGIN"
+	const authentificationInfos = await fetch("http://localhost:5678/api/users/login", {
+	method: "POST",
+	headers: {"Content-Type": "application/json"},
+	body: JSON.stringify(userInfos)
+  	});
+
+	// Récupération de la réponse et traduction
+	const authentificationResponse = await authentificationInfos.json();
+	// Stockage du TOKEN du User identifié et de l'état de l'authentification ---------
+	const authentificationToken = authentificationResponse.token;
+	const authentificationState = authentificationInfos.ok;
+	// --------------------------------------------------------------------------------
+
+	// Retour conditionnel en fonction de l'authentification et stockage des données dans le "sessionStorage".
+	if (authentificationState === true) {
+		sessionStorage.setItem("authentificationToken", authentificationToken);
+		sessionStorage.setItem("authentificationState", authentificationState);
+		window.location.replace("index.html");
+	} else {
+		sessionStorage.setItem("authentificationState", authentificationState);
+		const wrongUserNotification = document.querySelector(".wrong-user-notification");
+		wrongUserNotification.innerText = "Nom d'utilisateur ou mot de passe incorrect.";
+	};
+});
